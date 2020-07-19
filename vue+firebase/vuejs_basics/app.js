3aa4fba61c7029a4c98c604fb5af388b8dc0c80e
Djarid <djarid997@gmail.com>
2020-07-19 11:59:38 +0100

    lesson 15

--- vue+firebase/vuejs_basics/app.js
+++ vue+firebase/vuejs_basics/app.js
@@ -2,16 +2,14 @@ new Vue({
     el: '#app',
     data: {
         title: 'Becoming a Vue ninja',
-        showName: true,
-        showAge: true
+        items: ['Mushroom', 'Green Shells', 'Red Shells', 'banana', 'star'],
+        ninjas: [
+            {name: 'Crystal', age: 25, belt: 'black'},
+            {name: 'Ryu', age: 30, belt: 'brown'},
+            {name: 'Ken', age: 35, belt: 'orange'},
+        ]
     },
     methods: {
-        toggleName() {
-            this.showName = !this.showName
-        },
-        toggleAge() {
-            this.showAge = !this.showAge
-        }
 
     }
 })
