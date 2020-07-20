e1eb1bee8c58c3ba71a63dd468e286f3a5f0cb23
Djarid <djarid997@gmail.com>
2020-07-20 20:40:17 +0100

    Lesson 19

---
+++ vue+firebase/vue_cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      title: 'My first Vue app, woo :)'
+    }
+  },
+}
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
