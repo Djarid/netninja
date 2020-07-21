b3e7f7bbc55340917fa8000b9a4e07a8828a0368
Djarid <djarid997@gmail.com>
2020-07-21 07:41:23 +0100

    lesson 22

--- vue+firebase/vue_cli/src/App.vue
+++ vue+firebase/vue_cli/src/App.vue
@@ -2,30 +2,43 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <nav>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </nav>
+    <AllFriends  :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
+import AllFriends from './components/AllFriends.vue'
+import OnlineFriends from './components/OnlineFriends.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    OnlineFriends,
+    AllFriends
   },
   data() {
     return {
-      title: 'My first Vue app, woo :)'
+      title: 'My first Vue app, woo :)',
+      friends: [
+          { name: 'Mario', online: true },
+          { name: 'Luigi', online: false },
+          { name: 'Toad', online: true },
+          { name: 'Bowser', online: false },
+      ]
     }
   },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload)
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+      })
+    }
+  }
 }
 </script>
 
