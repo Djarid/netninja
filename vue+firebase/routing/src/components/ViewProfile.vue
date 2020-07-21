af1c5a54fe058c7e262d8307c52e69d8cd859f36
Djarid <djarid997@gmail.com>
2020-07-21 09:51:31 +0100

    lesson 31 - Watch Variables

--- vue+firebase/routing/src/components/ViewProfile.vue
+++ vue+firebase/routing/src/components/ViewProfile.vue
@@ -11,6 +11,14 @@ export default {
         return {
             userId: this.$route.params.user_id
         }
+    },
+    methods: {
+        updateID() {
+            this.userId = this.$route.params.user_id
+        }
+    },
+    watch: {
+        $route: 'updateId'
     }
 }
 </script>
