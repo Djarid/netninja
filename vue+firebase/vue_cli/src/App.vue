6e42bb487fa444f3cda5d75b1de2d5c56bfd9d41
Djarid <djarid997@gmail.com>
2020-07-21 08:06:08 +0100

    lesson 23

--- vue+firebase/vue_cli/src/App.vue
+++ vue+firebase/vue_cli/src/App.vue
@@ -2,42 +2,28 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends  :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
-import AllFriends from './components/AllFriends.vue'
-import OnlineFriends from './components/OnlineFriends.vue'
+import Blogs from './components/Blogs.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends
+    Blogs
   },
   data() {
     return {
       title: 'My first Vue app, woo :)',
-      friends: [
-          { name: 'Mario', online: true },
-          { name: 'Luigi', online: false },
-          { name: 'Toad', online: true },
-          { name: 'Bowser', online: false },
-      ]
     }
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+    
   }
 }
 </script>
---
+++ vue+firebase/vue_cli/src/components/Blogs.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="blogs">
+        <h2>{{ blogTitle }}</h2>
+        <button @click="changeTitle">Change Title</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blogs',
+    data() {
+        return {
+            blogTitle: 'Blogs'
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.blogTitle = "Amazing Blog Site"
+        }
+    },
+    beforeCreate() {
+        alert('beforeCreate hook')
+    },
+    created() {
+        alert('created hook')
+    },
+    beforeMount() {
+        // alert('beforeMount hook')
+    },
+    mounted() {
+        // alert('mounted hook')
+    },
+    beforeUpdate() {
+        alert('beforeUpdate hook')
+    },
+}
+</script>
