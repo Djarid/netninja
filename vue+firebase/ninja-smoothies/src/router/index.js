17b21d665098ce6b22a00c641eb96e6309365e1e
Djarid <djarid997@gmail.com>
2020-07-21 15:08:32 +0100

    lessone 47 - Add Smoothie component

--- vue+firebase/ninja-smoothies/src/router/index.js
+++ vue+firebase/ninja-smoothies/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
+import AddSmoothie from '@/components/AddSmoothie'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/add-smoothie',
+    name: 'AddSmoothie',
+    component: AddSmoothie
   }
 ]
 
