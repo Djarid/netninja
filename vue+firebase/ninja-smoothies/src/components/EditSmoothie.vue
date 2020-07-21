b239a865eee979c6a7e1bc33659be7f9718ffbc8
Djarid <djarid997@gmail.com>
2020-07-21 16:15:38 +0100

    lesson 54 - Edit Smoothie Form

--- vue+firebase/ninja-smoothies/src/components/EditSmoothie.vue
+++ vue+firebase/ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2 v-if="smoothie">Edit "{{ this.smoothie.title }}" Smoothie</h2>
-        <p v-else>Waiting for Smoothie recipe</p>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2 >Edit "{{ this.smoothie.title }}" Smoothie</h2>
+        <form @submit.prevent="EditSmoothie">
+            <div class="field title">
+                <label for="title">Smoothie Title:</label>
+                <input type="text" name="title" v-model="smoothie.title">
+            </div>
+            <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+                <label for="ingredient">Ingredients</label>
+                <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+                <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+            </div>
+            <div class="field add-ingredient">                
+                <label for="add-ingredient">Add an ingredient</label>
+                <input type="text" name="add-ingredient" @keydown.tab.prevent="AddIng" v-model="another">
+            </div>
+            <p v-if="feedback" class="red-text">{{ feedback }}</p>
+            <div class="field center-align">
+                <button class="btn pink">Update Smoothie</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -12,8 +30,29 @@ export default {
     name: 'EditSmoothie',
     data() {
         return {
-            smoothie: null
+            smoothie: null,
+            another: null,
+            feedback: null,
         }
+    },
+    methods: {
+        EditSmoothie() {
+            
+        },
+        AddIng() {
+            if (this.another) {
+                this.smoothie.ingredients.push(this.another)
+                this.another = ''
+                this.feedback = null
+            } else {
+                this.feedback = "You must enter a value to add an ingredient"
+            }
+        },
+        deleteIng(ing) {
+            this.smoothie.ingredients = this.smoothie.ingredients.filter(ingredient => {
+                return ingredient != ing
+            })
+        },
     },
     created() {
         let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
@@ -30,5 +69,25 @@ export default {
 
 
 <style>
-
+.edit-smoothie {
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+}
+.edit-smoothie h2 {
+    font-size: 2em;
+    margin-top: 20px auto;
+}
+.edit-smoothie .field {
+    margin: 20px auto;
+    position: relative;
+}
+.edit-smoothie .delete {
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+}
 </style>
