8531f11f366479011702605e914139e2e84ccc64
Djarid <djarid997@gmail.com>
2020-07-22 08:46:19 +0100

    lesson 69 - Add vue plugin (vue-chat-scroll)

--- vue+firebase/ninja-chat/src/views/Chat.vue
+++ vue+firebase/ninja-chat/src/views/Chat.vue
@@ -3,7 +3,7 @@
         <h2 class="center teal-text">Ninja Chat</h2>
         <div class="card">
             <div class="card-content">
-                <ul class="messages">
+                <ul class="messages" v-chat-scroll>
                     <li v-for="message in messages" :key="message.id">                        
                         <span class="name teal-text">{{ message.name }}</span>
                         <span class="content grey-text text-darken-3">{{ message.content }}</span>
@@ -67,5 +67,18 @@ export default {
 .chat .time {
     display: block;
     font-size: 0.8em;
+}
+.messages {
+    max-height: 300px;
+    overflow: auto;
+}
+.messages::-webkit-scrollbar {
+    width: 3px;
+}
+.messages::-webkit-scrollbar-track {
+    background: #dddddd
+}
+.messages::-webkit-scrollbar-thumb {
+    background: #aaaaaa
 }
 </style>
