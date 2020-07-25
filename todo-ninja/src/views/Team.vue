e33aba0c210af4fbfc615bf826db879f419513df
Djarid <djarid997@gmail.com>
2020-07-25 14:41:09 +0100

    Vuetify2 19 - Cards

--- todo-ninja/src/views/Team.vue
+++ todo-ninja/src/views/Team.vue
@@ -2,15 +2,38 @@
     <div class="team">
         <v-subheading class="grey--text">Team</v-subheading>
         <v-container  class="my-15">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur iste aliquid a, perspiciatis exercitationem cupiditate, laudantium nesciunt velit quasi dolorem, aspernatur neque accusamus commodi odit molestias cumque dolor asperiores! Minus?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod excepturi rem hic, consectetur minima mollitia cumque saepe itaque dolor. Facilis debitis distinctio sed illo quidem? Vitae tempore assumenda dignissimos illo.</p>
+            <v-row row wrap>
+                <v-col cols="12" xs="12" md="4" lg="3" class="text-center" v-for="person in team" :key="person.name">
+                    <v-card background="red" class="ma-3">
+                        <v-responsive class="pt-4">
+                            image here
+                        </v-responsive>                        
+                        <v-subheading>{{person.name}}</v-subheading>
+                        <div class="grey--text">{{person.role}}</div>
+                        <v-card-actions>
+                            <v-btn text color="grey">
+                                <v-icon small left>mdi-message</v-icon>
+                                <span>Message</span>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data: () => ({
+        team: [
+            { name: 'The Net Ninja', role: 'Web developer' },
+            { name: 'Ryu', role: 'Graphic designer' },
+            { name: 'Chun Li', role: 'Web developer' },
+            { name: 'Gouken', role: 'Social media maverick' },
+            { name: 'Yoshi', role: 'Sales guru'}
+        ]
+    })
 }
 </script>
 
