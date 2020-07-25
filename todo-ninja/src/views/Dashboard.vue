93e894990bd4cf1db36280801bc340af1220c1b5
Djarid <djarid997@gmail.com>
2020-07-25 14:28:17 +0100

    Vuetify2 18 - Tooltips

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -4,18 +4,14 @@
         <v-container  class="my-15">
 
             <v-row row class=mb-3>
-                <v-btn small text @click="sortBy('title')">
-                    <v-icon left small>mdi-folder</v-icon>
-                    <span class="caption text-lowercase">By project name</span>
-                </v-btn>
-                <v-btn small text @click="sortBy('person')">
-                    <v-icon left small>mdi-account</v-icon>
-                    <span class="caption text-lowercase">By person</span>
-                </v-btn>
-                <v-btn small text @click="sortDescBy('due')">
-                    <v-icon left small>mdi-calendar</v-icon>
-                    <span class="caption text-lowercase">By due date</span>
-                </v-btn>
+                <v-tooltip top v-for="(button, idx) in sortButtons" :key="idx">
+                    <template v-slot:activator="{ on }">
+                    <v-btn small text @click="sortBy(button.prop)" v-on="on">
+                        <v-icon left small>{{button.icon}}</v-icon>
+                        <span class="caption text-lowercase">{{button.label}}</span>
+                    </v-btn>
+                    </template>
+                </v-tooltip>
             </v-row>
 
             <v-card flat tile class="px-3" v-for="project in projects" :key="project.title">
@@ -45,16 +41,20 @@
 
 <script>
 export default {
-    data() {
-        return {
-            projects: [
-                { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-              ]            
-        }
-    },
+    name: 'dashboard',
+    data: () => ({
+        projects: [
+            { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        ],
+        sortButtons: [
+            { label: 'by project name', icon: 'mdi-folder', prop: 'title' },
+            { label: 'by person', icon: 'mdi-acount', prop: 'person' },
+            { label: 'by due date', icon: 'mdi-calendar', prop: 'due' },
+        ]
+    }),
     methods: {
         sortBy(prop) {
             this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
